4300168f8aced96d00c3387284311d770c325b70
Ievgenii Loza <ievgeniigenii@gmail.com>
2019-04-08 09:08:08 +0200

    Update colors

--- src/components/GeoHierarchy/d3/constants.js
+++ src/components/GeoHierarchy/d3/constants.js
@@ -3,9 +3,9 @@
 export const TRANSITION_DURATION = 400;
 
 // COLORS
-export const ROOT_COLOR = '#f57c00';
-export const NODE_COLOR = '#2F385E';
-export const CHILD_COLOR = '#ffbb1d';
+export const ROOT_COLOR = '#ff7140';
+export const NODE_COLOR = '#2f385e';
+export const CHILD_COLOR = '#c3c3c3';
 export const LINK_COLOR = '#c3c3c3';
-export const SELECTED_COLOR = '#f57c00';
+export const SELECTED_COLOR = '#ff7140';
 export const HOVER_COLOR = '#3B90E3';
