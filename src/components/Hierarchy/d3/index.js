b7d3bbcdbfce7c90c7a93f1ab5953121399bbfac
Ievgenii Loza <ievgeniigenii@gmail.com>
2019-05-09 14:16:26 +0200

    Remove Symbols

--- src/components/Hierarchy/d3/index.js
+++ src/components/Hierarchy/d3/index.js
@@ -2,6 +2,7 @@ import { hierarchy, tree } from 'd3-hierarchy';
 import { zoom, zoomIdentity, zoomTransform } from 'd3-zoom';
 import { event, select } from 'd3-selection';
 import { transform } from 'd3-transform';
+
 import 'd3-transition';
 
 import {
@@ -28,32 +29,6 @@ import {
 } from './constants';
 
 import { getTranslation, pathGenerator } from './lib';
-
-const _handleOnItemClick = Symbol('_handleOnItemClick');
-const _handleOnItemMouseOver = Symbol('_handleOnItemMouseOver');
-const _handleOnItemMouseLeave = Symbol('_handleOnItemMouseLeave');
-const _generateItems = Symbol('_generateItems');
-const _generateExistingItems = Symbol('_generateExistingItems');
-const _generateEnterItems = Symbol('_generateEnterItems');
-const _generateExitItems = Symbol('_generateExitItems');
-const _generateUpdateItems = Symbol('_generateUpdateItems');
-const _formatIndicators = Symbol('_formatIndicators');
-const _formatLabels = Symbol('_formatLabels');
-const _formatLinks = Symbol('_formatLinks');
-const _formatOverlays = Symbol('_formatOverlays');
-const _itemHasChildren = Symbol('_itemHasChildren');
-const _getConfig = Symbol('_getConfig');
-const _childrenKeySelector = Symbol('_childrenKeySelector');
-const _expandItems = Symbol('_expandItems');
-const _unselectNodes = Symbol('_unselectNodes');
-const _selectNode = Symbol('_selectNode');
-const _findSelectedPathItems = Symbol('_findSelectedPathItems');
-const _collapseDescendants = Symbol('_collapseDescendants');
-const _expandChildren = Symbol('_expandChildren');
-const _updateD3 = Symbol('_updateD3');
-const _zoomToItem = Symbol('_zoomToItem');
-const _zoomToPosition = Symbol('_zoomToPosition');
-const _init = Symbol('_init');
 
 class _d3 {
     constructor({
@@ -89,23 +64,23 @@ class _d3 {
         this.data = hierarchy({});
         this.searchQuery = searchQuery;
 
-        this[_init](root);
+        this._init(root);
     }
 
-    [_handleOnItemClick](item) {
-        this[_getConfig]('onItemClick')(item);
+    _handleOnItemClick(item) {
+        this._getConfig('onItemClick')(item);
 
-        if (this[_childrenKeySelector](item)) {
+        if (this._childrenKeySelector(item)) {
             this.resetSelection();
-            this[_collapseDescendants](item);
+            this._collapseDescendants(item);
         } else {
-            this[_expandChildren](item);
+            this._expandChildren(item);
         }
 
-        this[_updateD3](item);
+        this._updateD3(item);
     }
 
-    [_handleOnItemMouseOver](item) {
+    _handleOnItemMouseOver(item) {
         this.svg
             .selectAll('.item')
             .filter(
@@ -120,13 +95,13 @@ class _d3 {
                 select(this).raise();
             });
 
-        this[_formatLinks](
+        this._formatLinks(
             this.svg
                 .selectAll('.link')
                 .transition()
                 .duration(TRANSITION_DURATION)
         );
-        this[_formatIndicators](
+        this._formatIndicators(
             this.svg
                 .selectAll('.indicator')
                 .transition()
@@ -134,7 +109,7 @@ class _d3 {
         );
     }
 
-    [_handleOnItemMouseLeave](item) {
+    _handleOnItemMouseLeave(item) {
         this.svg
             .selectAll('.item')
             .filter(
@@ -146,13 +121,13 @@ class _d3 {
             )
             .attr('is-hover', null);
 
-        this[_formatLinks](
+        this._formatLinks(
             this.svg
                 .selectAll('.link')
                 .transition()
                 .duration(TRANSITION_DURATION)
         );
-        this[_formatIndicators](
+        this._formatIndicators(
             this.svg
                 .selectAll('.indicator')
                 .transition()
@@ -160,28 +135,28 @@ class _d3 {
         );
     }
 
-    [_generateItems](rootItem, data) {
+    _generateItems(rootItem, data) {
         const items = this.svg
             .select('.container')
             .selectAll('.item')
-            .data(data, d => d.data[this[_getConfig]('searchQueryKey')]);
+            .data(data, d => d.data[this._getConfig('searchQueryKey')]);
 
-        this[_generateExistingItems](items);
-        this[_generateExitItems](items, rootItem);
-        this[_generateUpdateItems](
+        this._generateExistingItems(items);
+        this._generateExitItems(items, rootItem);
+        this._generateUpdateItems(
             items,
-            this[_generateEnterItems](items, rootItem)
+            this._generateEnterItems(items, rootItem)
         );
     }
 
-    [_generateExistingItems](items) {
-        this[_formatLinks](items.selectAll('.link'));
-        this[_formatIndicators](items.selectAll('.indicator'));
-        this[_formatLabels](items.selectAll('.label'));
-        this[_formatOverlays](items.selectAll('.overlay'));
+    _generateExistingItems(items) {
+        this._formatLinks(items.selectAll('.link'));
+        this._formatIndicators(items.selectAll('.indicator'));
+        this._formatLabels(items.selectAll('.label'));
+        this._formatOverlays(items.selectAll('.overlay'));
     }
 
-    [_generateEnterItems](items, rootItem) {
+    _generateEnterItems(items, rootItem) {
         const enterItems = items
             .enter()
             .append('g')
@@ -202,22 +177,20 @@ class _d3 {
             .duration(TRANSITION_DURATION)
             .style('opacity', 1);
 
-        this[_formatLinks](
-            enterItems.append('path', 'g').attr('class', 'link')
-        );
+        this._formatLinks(enterItems.append('path', 'g').attr('class', 'link'));
 
-        this[_formatIndicators](
+        this._formatIndicators(
             enterItems.append('circle').attr('class', 'indicator')
         );
-        this[_formatLabels](enterItems.append('text').attr('class', 'label'));
-        this[_formatOverlays](
+        this._formatLabels(enterItems.append('text').attr('class', 'label'));
+        this._formatOverlays(
             enterItems.append('rect').attr('class', 'overlay')
         );
 
         return enterItems;
     }
 
-    [_generateExitItems](items, rootItem) {
+    _generateExitItems(items, rootItem) {
         const exitItems = items.exit();
 
         exitItems
@@ -253,13 +226,13 @@ class _d3 {
         return exitItems;
     }
 
-    [_generateUpdateItems](items, enterItems) {
+    _generateUpdateItems(items, enterItems) {
         const updateItems = enterItems.merge(items);
 
         updateItems
-            .on('click', item => this[_handleOnItemClick](item))
-            .on('mouseover', item => this[_handleOnItemMouseOver](item))
-            .on('mouseleave', item => this[_handleOnItemMouseLeave](item));
+            .on('click', item => this._handleOnItemClick(item))
+            .on('mouseover', item => this._handleOnItemMouseOver(item))
+            .on('mouseleave', item => this._handleOnItemMouseLeave(item));
 
         updateItems.attr('transform', d =>
             transform().translate(d => [d.y, d.x])({
@@ -268,12 +241,12 @@ class _d3 {
             })
         );
 
-        this[_formatLinks](updateItems.selectAll('.link'));
+        this._formatLinks(updateItems.selectAll('.link'));
 
         return updateItems;
     }
 
-    [_formatIndicators](indicators) {
+    _formatIndicators(indicators) {
         const that = this;
 
         return indicators
@@ -290,7 +263,7 @@ class _d3 {
                     return HOVER_COLOR;
                 }
 
-                if (!that[_itemHasChildren](d)) {
+                if (!that._itemHasChildren(d)) {
                     return LEAF_COLOR;
                 }
 
@@ -309,7 +282,7 @@ class _d3 {
                     return HOVER_COLOR;
                 }
 
-                if (!that[_itemHasChildren](d)) {
+                if (!that._itemHasChildren(d)) {
                     return LEAF_COLOR;
                 }
 
@@ -321,10 +294,10 @@ class _d3 {
             });
     }
 
-    [_formatLabels](labels) {
+    _formatLabels(labels) {
         return labels
             .attr('font-weight', d => {
-                if (!this[_itemHasChildren](d)) {
+                if (!this._itemHasChildren(d)) {
                     return 600;
                 }
 
@@ -334,10 +307,10 @@ class _d3 {
             .attr('x', 0)
             .style('fill', '#10183a')
             .attr('text-anchor', () => 'middle')
-            .text(this[_getConfig]('formatLabelText'));
+            .text(this._getConfig('formatLabelText'));
     }
 
-    [_formatLinks](links) {
+    _formatLinks(links) {
         return links
             .attr('fill', 'none')
             .attr('d', function(d) {
@@ -373,14 +346,14 @@ class _d3 {
                 });
             })
             .attr('stroke-dasharray', d =>
-                this[_itemHasChildren](d)
+                this._itemHasChildren(d)
                     ? null
-                    : this[_getConfig]('leafDashArraySize')
+                    : this._getConfig('leafDashArraySize')
             )
             .attr('stroke-width', 1.5);
     }
 
-    [_formatOverlays](node) {
+    _formatOverlays(node) {
         return node
             .attr('x', -25)
             .attr('y', -25)
@@ -390,34 +363,34 @@ class _d3 {
             .style('pointer-events', 'all');
     }
 
-    [_itemHasChildren](item) {
-        if (!Array.isArray(item.data[this[_getConfig]('childrenKey')])) {
+    _itemHasChildren(item) {
+        if (!Array.isArray(item.data[this._getConfig('childrenKey')])) {
             return false;
         }
 
-        if (item.data[this[_getConfig]('childrenKey')].length === 0) {
+        if (item.data[this._getConfig('childrenKey')].length === 0) {
             return false;
         }
 
         return true;
     }
 
-    [_getConfig](value) {
+    _getConfig(value) {
         return this.config[value];
     }
 
-    [_childrenKeySelector](d) {
-        return d[this[_getConfig]('childrenKey')];
+    _childrenKeySelector(d) {
+        return d[this._getConfig('childrenKey')];
     }
 
-    [_expandItems](items) {
+    _expandItems(items) {
         for (let i = 0, len = items.length; i < len; i += 1) {
-            this[_expandChildren](items[i]);
-            this[_updateD3](items[i]);
+            this._expandChildren(items[i]);
+            this._updateD3(items[i]);
         }
     }
 
-    [_unselectNodes]() {
+    _unselectNodes() {
         this.svg
             .selectAll('.item')
             .filter(function() {
@@ -425,11 +398,11 @@ class _d3 {
             })
             .attr('is-selected', null);
 
-        this[_formatLinks](this.svg.selectAll('.link'));
-        this[_formatIndicators](this.svg.selectAll('.indicator'));
+        this._formatLinks(this.svg.selectAll('.link'));
+        this._formatIndicators(this.svg.selectAll('.indicator'));
     }
 
-    [_selectNode](d) {
+    _selectNode(d) {
         this.svg
             .selectAll('.item')
             .filter(
@@ -444,14 +417,14 @@ class _d3 {
                 select(this).raise();
             });
 
-        this[_formatLinks](this.svg.selectAll('.link'));
-        this[_formatIndicators](this.svg.selectAll('.indicator'));
+        this._formatLinks(this.svg.selectAll('.link'));
+        this._formatIndicators(this.svg.selectAll('.indicator'));
     }
 
-    [_findSelectedPathItems](node, searchString, pathArray) {
+    _findSelectedPathItems(node, searchString, pathArray) {
         if (
             String(
-                node.data[this[_getConfig]('searchQueryKey')]
+                node.data[this._getConfig('searchQueryKey')]
             ).toLocaleLowerCase() === searchString.toLocaleLowerCase()
         ) {
             pathArray.push(node);
@@ -459,9 +432,9 @@ class _d3 {
             return pathArray;
         }
 
-        const children = node[this[_getConfig]('childrenKey')]
-            ? node[this[_getConfig]('childrenKey')]
-            : node[`_${this[_getConfig]('childrenKey')}`];
+        const children = node[this._getConfig('childrenKey')]
+            ? node[this._getConfig('childrenKey')]
+            : node[`_${this._getConfig('childrenKey')}`];
 
         if (!children) {
             return false;
@@ -470,7 +443,7 @@ class _d3 {
         for (let i = 0; i < children.length; i += 1) {
             pathArray.push(node);
 
-            const found = this[_findSelectedPathItems](
+            const found = this._findSelectedPathItems(
                 children[i],
                 searchString,
                 pathArray
@@ -486,43 +459,43 @@ class _d3 {
         return null;
     }
 
-    [_collapseDescendants](node) {
+    _collapseDescendants(node) {
         const collapsedNode = node;
 
-        if (this[_childrenKeySelector](collapsedNode)) {
-            collapsedNode[`_${this[_getConfig]('childrenKey')}`] = this[
-                _childrenKeySelector
-            ](collapsedNode);
-            collapsedNode[`_${this[_getConfig]('childrenKey')}`].forEach(
-                child => this[_collapseDescendants](child)
+        if (this._childrenKeySelector(collapsedNode)) {
+            collapsedNode[
+                `_${this._getConfig('childrenKey')}`
+            ] = this._childrenKeySelector(collapsedNode);
+            collapsedNode[`_${this._getConfig('childrenKey')}`].forEach(child =>
+                this._collapseDescendants(child)
             );
-            collapsedNode[this[_getConfig]('childrenKey')] = null;
+            collapsedNode[this._getConfig('childrenKey')] = null;
         }
 
         return collapsedNode;
     }
 
-    [_expandChildren](node) {
+    _expandChildren(node) {
         const expandedNode = node;
 
-        if (this[_childrenKeySelector](expandedNode) === null) {
-            expandedNode[this[_getConfig]('childrenKey')] =
-                expandedNode[`_${this[_getConfig]('childrenKey')}`];
-            expandedNode[`_${this[_getConfig]('childrenKey')}`] = null;
+        if (this._childrenKeySelector(expandedNode) === null) {
+            expandedNode[this._getConfig('childrenKey')] =
+                expandedNode[`_${this._getConfig('childrenKey')}`];
+            expandedNode[`_${this._getConfig('childrenKey')}`] = null;
         }
 
         return expandedNode;
     }
 
-    [_updateD3](rootItem = this.data) {
-        this[_generateItems](
+    _updateD3(rootItem = this.data) {
+        this._generateItems(
             rootItem,
             this.tree(this.data)
                 .descendants()
                 .map(node => {
                     const updatedNode = node;
                     updatedNode.y =
-                        updatedNode.depth * this[_getConfig]('nodeDistance');
+                        updatedNode.depth * this._getConfig('nodeDistance');
                     updatedNode.x0 = node.x;
                     updatedNode.y0 = node.y;
 
@@ -531,15 +504,15 @@ class _d3 {
         );
     }
 
-    [_zoomToItem](item) {
+    _zoomToItem(item) {
         setTimeout(() => {
             const { y, x } = select(item).node();
-            this[_selectNode](item);
-            this[_zoomToPosition](y, x, 1.5);
+            this._selectNode(item);
+            this._zoomToPosition(y, x, 1.5);
         }, TRANSITION_DURATION);
     }
 
-    [_zoomToPosition](y, x, scale) {
+    _zoomToPosition(y, x, scale) {
         this.svg
             .call(this.zoom)
             .transition()
@@ -548,23 +521,23 @@ class _d3 {
                 this.zoom.transform,
                 zoomIdentity
                     .translate(
-                        -y * scale + this[_getConfig]('width') / 2,
-                        -x * scale + this[_getConfig]('height') / 2
+                        -y * scale + this._getConfig('width') / 2,
+                        -x * scale + this._getConfig('height') / 2
                     )
                     .scale(scale)
             );
     }
 
-    [_init](root) {
+    _init(root) {
         this.svg = root
             .append('svg')
-            .attr('class', this[_getConfig]('svgClass'))
+            .attr('class', this._getConfig('svgClass'))
             .style('pointer-events', 'all');
 
         this.svg.append('g').attr('class', 'container');
 
         this.zoom = zoom()
-            .scaleExtent(this[_getConfig]('scaleExtent'))
+            .scaleExtent(this._getConfig('scaleExtent'))
             .on('zoom', () => {
                 this.svg
                     .select('.container')
@@ -573,19 +546,19 @@ class _d3 {
     }
 
     updateData(data, expandTree = false) {
-        this.data = hierarchy(data, this[_getConfig]('childrenKeySelector'));
+        this.data = hierarchy(data, this._getConfig('childrenKeySelector'));
 
         if (!expandTree) {
-            this[_collapseDescendants](this.data);
+            this._collapseDescendants(this.data);
         }
 
-        this[_zoomToPosition](0, 0, 1);
+        this._zoomToPosition(0, 0, 1);
 
         if (this.searchQuery !== null) {
             this.setSelection(this.searchQuery);
         }
 
-        this[_updateD3]();
+        this._updateD3();
     }
 
     updateDimensions() {
@@ -598,7 +571,7 @@ class _d3 {
     }
 
     setSelection(selected) {
-        const items = this[_findSelectedPathItems](
+        const items = this._findSelectedPathItems(
             this.data,
             String(selected),
             []
@@ -606,41 +579,41 @@ class _d3 {
 
         if (items) {
             this.searchQuery = String(selected);
-            this[_unselectNodes]();
-            this[_expandItems](items);
-            this[_zoomToItem](items.slice(-1)[0]);
+            this._unselectNodes();
+            this._expandItems(items);
+            this._zoomToItem(items.slice(-1)[0]);
         }
     }
 
     resetSelection() {
         this.searchQuery = null;
 
-        this[_unselectNodes]();
-        this[_updateD3]();
+        this._unselectNodes();
+        this._updateD3();
 
-        this[_getConfig]('onSelectionClear')();
+        this._getConfig('onSelectionClear')();
     }
 
     reload() {
         this.searchQuery = null;
 
-        this[_unselectNodes]();
+        this._unselectNodes();
 
-        if (this.data[this[_getConfig]('childrenKey')]) {
-            this.data[this[_getConfig]('childrenKey')].forEach(child =>
-                this[_collapseDescendants](child)
+        if (this.data[this._getConfig('childrenKey')]) {
+            this.data[this._getConfig('childrenKey')].forEach(child =>
+                this._collapseDescendants(child)
             );
         }
 
-        this[_zoomToPosition](0, 0, 1);
-        this[_updateD3]();
-        this[_getConfig]('onSelectionClear')();
+        this._zoomToPosition(0, 0, 1);
+        this._updateD3();
+        this._getConfig('onSelectionClear')();
     }
 
     resetZoom() {
         if (this.searchQuery) {
-            return this[_zoomToItem](
-                this[_findSelectedPathItems](
+            return this._zoomToItem(
+                this._findSelectedPathItems(
                     this.data,
                     this.searchQuery,
                     []
@@ -648,19 +621,19 @@ class _d3 {
             );
         }
 
-        this[_zoomToPosition](0, 0, 1);
+        this._zoomToPosition(0, 0, 1);
     }
 
     zoomIn() {
         const { k: scale } = zoomTransform(this.svg.node());
 
-        this.zoom.scaleTo(this.svg, scale + this[_getConfig]('scaleStep'));
+        this.zoom.scaleTo(this.svg, scale + this._getConfig('scaleStep'));
     }
 
     zoomOut() {
         const { k: scale } = zoomTransform(this.svg.node());
 
-        this.zoom.scaleTo(this.svg, scale - this[_getConfig]('scaleStep'));
+        this.zoom.scaleTo(this.svg, scale - this._getConfig('scaleStep'));
     }
 }
 
